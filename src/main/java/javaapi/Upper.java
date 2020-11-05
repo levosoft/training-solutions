3acbb7f6fb3a5ddbf316adcc1f122a4c7ff8d4ec
levosoft <lukacs.levente.geza@gmail.com>
2020-11-05 13:37:14 +0100

    toUpperCase

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String text = "HelloWorld";
+        System.out.println(text.toUpperCase());
+    }
+
+}
