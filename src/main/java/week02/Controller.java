83e9122247462bb4d8d7e292a0bf7cc9e548b37f
levosoft <lukacs.levente.geza@gmail.com>
2020-11-08 23:14:55 +0100

    Projektmunka - 5.szakasz

--- src/main/java/week02/Controller.java
+++ src/main/java/week02/Controller.java
@@ -28,20 +28,68 @@ public class Controller {
 
         for (int i = 0; i <= numberOfMeetingrooms; i++) {
 
-            System.out.println("Add meg a tárgyaló nevét");
+            System.out.println("Add meg a tárgyaló nevét:");
             name = scanner.nextLine();
 
-            System.out.println("Add meg a tárgyaló szélességét");
+            System.out.println("Add meg a tárgyaló szélességét:");
             width = scanner.nextInt();
             System.out.println();
 
-            System.out.println("Add meg a tárgyaló hosszát");
+            System.out.println("Add meg a tárgyaló hosszát:");
             length = scanner.nextInt();
             System.out.println();
 
             MeetingRoom meetingRoom = new MeetingRoom(name, length, width);
             office.addMeetingRoom(meetingRoom);
         }
+
+        printMenu();
+    }
+
+    public static void printMenu(){
+        System.out.println("==================================");
+        System.out.println("              MENÜ                ");
+        System.out.println("1. Tárgyalók sorrendben");
+        System.out.println("2. Tárgyalók visszafele sorrendben");
+        System.out.println("3. Minden második tárgyaló");
+        System.out.println("4. Területek");
+        System.out.println("5. Keresés pontos név alapján");
+        System.out.println("6. Keresés névtöredék alapján");
+        System.out.println("7. Keresés terület alapján");
+        System.out.println("==================================");
+
+        runMenu();
+    }
+
+    public static void runMenu(){
+        Scanner scanner = new Scanner(System.in);
+        int nr = scanner.nextInt();
+        System.out.println();
+
+        if(nr == 1){
+            office.printNames();
+        }else if(nr == 2){
+            office.printNamesReverse();
+        }else if(nr == 3){
+            office.printEvenNames();
+        }else if(nr == 4){
+            office.printAreas();
+        }else if(nr == 5){
+            System.out.println("Add meg a keresett nevet:");
+            String name = scanner.nextLine();
+            office.printMeetingRoomsWithName(name);
+        }else if(nr == 6){
+            System.out.println("Add meg a keresett névtöredéket:");
+            String part = scanner.nextLine();
+            office.printMeetingRoomsContains(part);
+        }else if(nr == 7){
+            System.out.println("Add meg a keresett területet:");
+            int area = scanner.nextInt();
+            System.out.println();
+            office.printAreasLargerThan(area);
+        }
+
+        printMenu();
     }
 
 
