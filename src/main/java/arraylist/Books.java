d61e78530ad9640e225954604ec7b5a74c065301
levosoft <lukacs.levente.geza@gmail.com>
2020-11-13 21:52:22 +0100

    Lista műveletek -> Foreach ciklus keres a listában, a találatokból új listát generál

--- src/main/java/arraylist/Books.java
+++ src/main/java/arraylist/Books.java
@@ -1,4 +1,44 @@
 package arraylist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Books {
+    List<String> titles = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Books books = new Books();
+
+        books.add("Arany ember");
+        books.add("Téli berek");
+        books.add("A láthatatlan ember");
+        books.add("Rokonok");
+        books.add("Télapó itt van");
+
+        System.out.println(books.findAllByPrefix("Tél").toString());
+
+        System.out.println(books.getTitles().toString());
+    }
+
+    public void add(String title){
+        titles.add(title);
+    }
+
+    public List<String> findAllByPrefix(String prefix){
+        List<String> matchingTitle = new ArrayList<>();
+
+        int i = prefix.length();
+
+        for (String title: titles){
+            if(title.substring(0,i).equals(prefix)){
+                matchingTitle.add(title);
+            }
+        }
+        return matchingTitle;
+    }
+
+    public List<String> getTitles(){
+        return titles;
+    }
+
 }
