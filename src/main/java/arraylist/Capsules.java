58027634a56bd0df7ea49eab78cb0184e8d9af8e
levosoft <lukacs.levente.geza@gmail.com>
2020-11-13 20:57:08 +0100

    Lista műveletek -> List->ArrayList

--- src/main/java/arraylist/Capsules.java
+++ src/main/java/arraylist/Capsules.java
@@ -33,8 +33,8 @@ public class Capsules {
     }
 
     public void removeLast(){
-        int lastIndex = capsules.size();
-        capsules.remove(lastIndex-1);
+        int lastIndex = capsules.size() - 1;
+        capsules.remove(lastIndex);
     }
 
     public List<String> getColors(){
