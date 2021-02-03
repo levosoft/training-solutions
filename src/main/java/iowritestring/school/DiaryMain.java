6dcdecc9921efc19f4f1f4cb9ec004c96310fa22
levosoft <lukacs.levente.geza@gmail.com>
2021-02-03 20:49:25 +0100

    Új fájl létrehozása / meglévő fájlba írás Beolvasás soronként Listába Sorok utolsó karaktérének Integerként való kezelése (átlagszámítás)

--- src/main/java/iowritestring/school/DiaryMain.java
+++ src/main/java/iowritestring/school/DiaryMain.java
@@ -1,6 +1,7 @@
 package iowritestring.school;
 
 public class DiaryMain {
+    //Tesztelésre
     public static void main(String[] args) {
         Diary diary = new Diary();
         diary.newMark("Janicsák Veca", 5);
