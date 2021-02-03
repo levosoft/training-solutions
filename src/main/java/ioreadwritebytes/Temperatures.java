a95a3f7deb9648a22acdcd81c548273b3afc5cc7
levosoft <lukacs.levente.geza@gmail.com>
2021-02-03 20:51:40 +0100

    - Bináris fájl olvasása byte[]-be - Bináris fájl írása byte[]-ből

---
+++ src/main/java/ioreadwritebytes/Temperatures.java
@@ -0,0 +1,27 @@
+package ioreadwritebytes;
+
+public class Temperatures {
+
+    private byte[] data;
+
+    public Temperatures(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public double  getYearAverage(){
+        double yearAverage = 0;
+
+        return yearAverage;
+    }
+
+    public double getMonthAverage(){
+        double monthAverage = 0;
+
+        return monthAverage;
+    }
+
+}
