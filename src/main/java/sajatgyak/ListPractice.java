1130b6b1601328629b84ec10d008361d958f6f2c
levosoft <lukacs.levente.geza@gmail.com>
2021-02-10 16:21:32 +0100

    Lista inicializálása, elemeinek megadásával

---
+++ src/main/java/sajatgyak/ListPractice.java
@@ -0,0 +1,15 @@
+package sajatgyak;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListPractice {
+
+    public static void main(String[] args) {
+        List<String> myList1 = Arrays.asList("alma", "körte", "szilva");
+        List<String> myList2 = List.of("barack", "málna", "meggy");         //Ez a korszerűbb (JAVA 9-óta)
+
+        System.out.println(myList1);
+        System.out.println(myList2);
+    }
+}
