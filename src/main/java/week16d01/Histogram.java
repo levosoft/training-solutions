ecbd028b5a4f1366b58da5d7aac8f9ab6d7ebcb2
levosoft <lukacs.levente.geza@gmail.com>
2021-02-16 15:02:14 +0100

    Feladatleirás pótolva

--- src/main/java/week16d01/Histogram.java
+++ src/main/java/week16d01/Histogram.java
@@ -6,6 +6,24 @@ import java.io.StringWriter;
 
 public class Histogram {
 
+    /*Olvass be egy szöveges fájlt soronként, melyben soronkont számok vannak. Majd hozz létre egy stringet, mely ugyanennyi sort tartalmaz, és annyi csillagot ír ki
+    egymás mellé soronkont, amennyi a bemeneti fájlban lévő szám.
+
+    Azaz a bemeneti fájl:
+    3
+    4
+    1
+    2
+
+    A létrehozott String:
+    ***
+    ****
+    *
+    **
+    Figyelj, hogy az algoritmusod lehetőleg hatékony legyen!
+
+    A Histogram osztályba dolgozz, tesztje HistogramTest. Minden sor után legyen sortörés, az utolsó után is.*/
+
     public String createHistogram(BufferedReader reader){
 
         int x;
