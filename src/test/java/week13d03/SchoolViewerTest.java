1bc3986708d9fb3454e1703b58b00e273a474e98
levosoft <lukacs.levente.geza@gmail.com>
2021-02-16 17:54:19 +0100

    Teszteset pontosítva

--- src/test/java/week13d03/SchoolViewerTest.java
+++ src/test/java/week13d03/SchoolViewerTest.java
@@ -2,12 +2,15 @@ package week13d03;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SchoolViewerTest {
 
     @Test
     public void test1(){
 
         SchoolViewer sw = new SchoolViewer();
-        System.out.println(sw.lessonNumByTeacher("Csincsilla Csilla"));
+        //System.out.println(sw.lessonNumByTeacher("Csincsilla Csilla"));
+        assertEquals(22, sw.lessonNumByTeacher("Csincsilla Csilla"));
     }
 }
