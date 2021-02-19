461f0f952e309e8f1b3f8bd5223168eab5775a83
levosoft <lukacs.levene.geza@gmail.com>
2021-02-19 22:56:11 +0100

    .

--- src/main/java/sajatgyak/StringPractice.java
+++ src/main/java/sajatgyak/StringPractice.java
@@ -1,13 +1,18 @@
 package sajatgyak;
 
-public class StringSplitPractice {
+public class StringPractice {
 
     public static void main(String[] args) {
 
+        //SPLIT
         String name = "John:Doe";
         String[] names = name.split(":");
 
         System.out.println(names[0]);
         System.out.println(names[1]);
+
+
+        //REPEAT
+        System.out.println("*".repeat(3));
     }
 }
