c5f525bedbae02d4ccfc8de9185b4c3a308d9fbc
levosoft <lukacs.levente.geza@gmail.com>
2021-02-22 17:46:45 +0100

    .

--- src/main/java/sajatgyak/ListPractice.java
+++ src/main/java/sajatgyak/ListPractice.java
@@ -1,11 +1,14 @@
 package sajatgyak;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ListPractice {
 
     public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<>();
         List<String> myList1 = Arrays.asList("alma", "körte", "szilva");
         List<String> myList2 = List.of("barack", "málna", "meggy");         //Ez a korszerűbb (JAVA 9-óta)
 
---
+++ src/main/java/sajatgyak/SetPractice.java
@@ -0,0 +1,14 @@
+package sajatgyak;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetPractice {
+
+
+    
+    public static void main(String[] args) {
+
+        Set<String> mySet1 = new HashSet<>();
+    }
+}
