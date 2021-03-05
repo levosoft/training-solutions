239fd617362a7d54c81112f87769d7ae9541e78d
Lukács Levente <lukacs.levente@helit.hu>
2021-03-05 13:56:26 +0100

    Gyakorlás

--- src/main/java/sajatgyak/Practice.java
+++ src/main/java/sajatgyak/Practice.java
@@ -1,6 +1,10 @@
 package sajatgyak;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.Collator;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -14,9 +18,31 @@ public class Practice {
         Collections.sort(nevek, Collator.getInstance());
         System.out.println(nevek);
 
+        //Fájl beolvasás Classpath-ról (resources/package)
+        BufferedReader reader = new BufferedReader(new InputStreamReader(Practice.class.getResourceAsStream("myFile.txt")));
+        System.out.println(new Practice().getReader(reader));
 
+    }
 
+    //File olvasása soronként - Paraméter reader!
+    public List<String> getReader(BufferedReader reader) {
 
+        List<String> myList = new ArrayList<>();
+        String id;
+        String line;
+
+        try(reader){
+            while ((line = reader.readLine()) != null){
+                if(line.contains("ID")){
+                    String[] datas = line.split(":");
+                    myList.add(datas[1]);
+                }
+            }
+            return myList;
+        }
+        catch (IOException ioe){
+            throw new IllegalStateException("Can not read file!", ioe);
+        }
     }
 
 }
